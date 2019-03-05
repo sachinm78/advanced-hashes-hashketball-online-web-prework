e2ad718f337149073e9bc89bbdca66661ef24aa8
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 17:13:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,7 +9,7 @@ def game_hash
           :number => 0,
           :shoe => 16,
           :points => 22,
-          :rebounds => 22,
+          :rebounds => 12,
           :assists => 12,
           :steals => 3,
           :blocks => 1,
