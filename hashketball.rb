f2673d4fb55d45e42b5c6cab5fa2a515a8f00115
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 16:38:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,13 +64,13 @@ def game_hash
       :players => {
         "Jeff Adrien" => {
         :number => 4 
-        :shoe => 16
-        :points
-        :rebounds
-        :assists
-        :steals
-        :blocks
-        :slam_dunks
+        :shoe => 18
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2,
       }
             }
     }
