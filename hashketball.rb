c04753aff6e42880755c580c60486528cf04d018
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 16:33:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,8 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets"
       :colors => ["Black", "White"]
-      :players => {name => {
+      :players => {
+        
         :number
         :shoe
         :points
